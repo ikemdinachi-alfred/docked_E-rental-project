59a15d4acee23a4eb711ca69eea67020eb0ac39c
Ike <patrickikechukwukalukalu@gmail.com>
2024-11-29 07:44:29 +0100

    added hello word

--- src/main/java/com/alfredTech/e_rental_application/ERentalApplication.java
+++ src/main/java/com/alfredTech/e_rental_application/ERentalApplication.java
@@ -2,9 +2,17 @@ package com.alfredTech.e_rental_application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ERentalApplication {
+	@PostMapping("word")
+	public String sayHello(){
+		return "Hello World";
+
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ERentalApplication.class, args);
